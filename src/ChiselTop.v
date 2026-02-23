f5d308b86f9b338e7320847566475b817435ad49
Maximillian-Udar <122749236+Maximillian-Udar@users.noreply.github.com>
2026-02-23 14:15:13 +0100

    seven segment 4-bit adder

--- src/ChiselTop.v
+++ src/ChiselTop.v
@@ -1,36 +1,48 @@
 module ChiselTop(
-  input        clock,
-  input        reset,
-  input  [7:0] io_ui_in, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 8:14]
-  output [7:0] io_uo_out, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 8:14]
-  input  [7:0] io_uio_in, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 8:14]
-  output [7:0] io_uio_out, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 8:14]
-  output [7:0] io_uio_oe // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 8:14]
+  input         clock,
+  input         reset,
+  input  [15:0] io_sw, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 5:14]
+  output [6:0]  io_seg, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 5:14]
+  output [3:0]  io_an, // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 5:14]
+  output        io_dp // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 5:14]
 );
 `ifdef RANDOMIZE_REG_INIT
   reg [31:0] _RAND_0;
-  reg [31:0] _RAND_1;
 `endif // RANDOMIZE_REG_INIT
-  wire [7:0] _add_T_1 = io_ui_in + io_uio_in; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 22:19]
-  reg [31:0] cntReg; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 25:23]
-  reg  ledReg; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 26:23]
-  wire [31:0] _cntReg_T_1 = cntReg + 32'h1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 27:20]
-  wire [6:0] add = _add_T_1[6:0]; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 21:24 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 22:7]
-  assign io_uo_out = {ledReg,add}; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23]
-  assign io_uio_out = 8'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 16:14]
-  assign io_uio_oe = 8'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 19:13]
+  wire [3:0] a = io_sw[3:0]; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 13:16]
+  wire [3:0] b = io_sw[7:4]; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 14:16]
+  wire [4:0] sum = a + b; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 15:15]
+  reg [16:0] refreshCounter; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 19:31]
+  wire [16:0] _refreshCounter_T_1 = refreshCounter + 17'h1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 20:36]
+  wire [1:0] digitSelect = refreshCounter[16:15]; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 23:35]
+  wire [3:0] _digitData_T_2 = {3'h0,sum[4]}; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 39:29]
+  wire [3:0] _GEN_0 = 2'h3 == digitSelect ? 4'h7 : 4'hf; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 30:9 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 46:17]
+  wire [3:0] _GEN_2 = 2'h2 == digitSelect ? 4'hb : _GEN_0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 42:17]
+  wire [3:0] _GEN_4 = 2'h1 == digitSelect ? 4'hd : _GEN_2; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 38:17]
+  wire [3:0] _GEN_5 = 2'h1 == digitSelect ? _digitData_T_2 : 4'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 39:17]
+  wire [3:0] digitData = 2'h0 == digitSelect ? sum[3:0] : _GEN_5; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 35:17]
+  wire [6:0] _decoder_T_1 = 4'h1 == digitData ? 7'h79 : 7'h40; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_3 = 4'h2 == digitData ? 7'h24 : _decoder_T_1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_5 = 4'h3 == digitData ? 7'h30 : _decoder_T_3; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_7 = 4'h4 == digitData ? 7'h19 : _decoder_T_5; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_9 = 4'h5 == digitData ? 7'h12 : _decoder_T_7; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_11 = 4'h6 == digitData ? 7'h2 : _decoder_T_9; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_13 = 4'h7 == digitData ? 7'h78 : _decoder_T_11; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_15 = 4'h8 == digitData ? 7'h0 : _decoder_T_13; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_17 = 4'h9 == digitData ? 7'h10 : _decoder_T_15; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_19 = 4'ha == digitData ? 7'h8 : _decoder_T_17; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_21 = 4'hb == digitData ? 7'h3 : _decoder_T_19; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_23 = 4'hc == digitData ? 7'h46 : _decoder_T_21; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_25 = 4'hd == digitData ? 7'h21 : _decoder_T_23; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  wire [6:0] _decoder_T_27 = 4'he == digitData ? 7'h6 : _decoder_T_25; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  assign io_seg = 4'hf == digitData ? 7'he : _decoder_T_27; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 55:48]
+  assign io_an = 2'h0 == digitSelect ? 4'he : _GEN_4; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 32:23 C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 34:17]
+  assign io_dp = 1'h1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 63:10]
   always @(posedge clock) begin
-    if (reset) begin // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 25:23]
-      cntReg <= 32'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 25:23]
-    end else if (cntReg == 32'h17d7840) begin // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 28:32]
-      cntReg <= 32'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 29:12]
+    if (reset) begin // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 19:31]
+      refreshCounter <= 17'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 19:31]
     end else begin
-      cntReg <= _cntReg_T_1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 27:10]
-    end
-    if (reset) begin // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 26:23]
-      ledReg <= 1'h0; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 26:23]
-    end else if (cntReg == 32'h17d7840) begin // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 28:32]
-      ledReg <= ~ledReg; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 30:12]
+      refreshCounter <= _refreshCounter_T_1; // @[C:\\Users\\maxim\\My Drive\\Uni\\2 Sem\\Digi2\\TT-Chip\\src\\main\\scala\\ChiselTop.scala 20:18]
     end
   end
 // Register and memory initialization
@@ -70,9 +82,7 @@ initial begin
     `endif
 `ifdef RANDOMIZE_REG_INIT
   _RAND_0 = {1{`RANDOM}};
-  cntReg = _RAND_0[31:0];
-  _RAND_1 = {1{`RANDOM}};
-  ledReg = _RAND_1[0:0];
+  refreshCounter = _RAND_0[16:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
--- src/main/scala/ChiselTop.scala
+++ src/main/scala/ChiselTop.scala
@@ -1,35 +1,66 @@
 import chisel3._
+import chisel3.util._
 
-/**
- * Example design in Chisel.
- * A redesign of the Tiny Tapeout example.
- */
-class ChiselTop() extends Module {
+class ChiselTop extends Module {
   val io = IO(new Bundle {
-    val ui_in = Input(UInt(8.W))      // Dedicated inputs
-    val uo_out = Output(UInt(8.W))    // Dedicated outputs
-    val uio_in = Input(UInt(8.W))     // IOs: Input path
-    val uio_out = Output(UInt(8.W))   // IOs: Output path
-    val uio_oe = Output(UInt(8.W))    // IOs: Enable path (active high: 0=input, 1=output)
+    val sw  = Input(UInt(16.W))      // 16 Switches
+    val seg = Output(UInt(7.W))      // 7 Segments (Cathodes)
+    val an  = Output(UInt(4.W))      // 4 Anodes (Digit Select)
+    val dp  = Output(UInt(1.W))      // Decimal Point
   })
 
-  io.uio_out := 0.U
-  // something
-  // use bi-directionals as input
-  io.uio_oe := 0.U
+  // --- 1. The 4-Bit Adder Logic ---
+  val a = io.sw(3, 0)                // First 4-bit number (Switches 0-3)
+  val b = io.sw(7, 4)                // Second 4-bit number (Switches 4-7)
+  val sum = a +& b                   // 5-bit result
 
-  val add = WireDefault(0.U(7.W))
-  add := io.ui_in + io.uio_in
+  // --- 2. Display Multiplexing Logic ---
+  // Counter to scan through digits
+  val refreshCounter = RegInit(0.U(17.W))
+  refreshCounter := refreshCounter + 1.U
 
-  // Blink with 1 Hz
-  val cntReg = RegInit(0.U(32.W))
-  val ledReg = RegInit(0.U(1.W))
-  cntReg := cntReg + 1.U
-  when (cntReg === 25000000.U) {
-    cntReg := 0.U
-    ledReg := ~ledReg
+  // Use top 2 bits to select active digit
+  val digitSelect = refreshCounter(16, 15)
+
+  // --- 3. Digit Selection & Data Routing ---
+  val digitData = Wire(UInt(4.W))
+  digitData := 0.U // *** FIX: Initialize wire to 0 to satisfy compiler ***
+
+  // Default: all anodes OFF
+  io.an := "b1111".U
+
+  switch(digitSelect) {
+    is(0.U) { // Rightmost digit
+      io.an     := "b1110".U
+      digitData := sum(3, 0)
+    }
+    is(1.U) { // Second digit
+      io.an     := "b1101".U
+      digitData := 0.U(3.W) ## sum(4)
+    }
+    is(2.U) { // Third digit
+      io.an     := "b1011".U
+      digitData := 0.U
+    }
+    is(3.U) { // Leftmost digit
+      io.an     := "b0111".U
+      digitData := 0.U
+    }
   }
-  io.uo_out := ledReg ## add
+
+  // --- 4. Seven-Segment Decoder ---
+  val decoder = Wire(UInt(7.W))
+
+  // 0 -> 1000000 (0x40), etc. (Active LOW)
+  decoder := MuxLookup(digitData, "b1111111".U)(Seq(
+    0.U  -> "b1000000".U, 1.U  -> "b1111001".U, 2.U  -> "b0100100".U, 3.U  -> "b0110000".U,
+    4.U  -> "b0011001".U, 5.U  -> "b0010010".U, 6.U  -> "b0000010".U, 7.U  -> "b1111000".U,
+    8.U  -> "b0000000".U, 9.U  -> "b0010000".U, 10.U -> "b0001000".U, 11.U -> "b0000011".U,
+    12.U -> "b1000110".U, 13.U -> "b0100001".U, 14.U -> "b0000110".U, 15.U -> "b0001110".U
+  ))
+
+  io.seg := decoder
+  io.dp  := 1.U
 }
 
 object ChiselTop extends App {
